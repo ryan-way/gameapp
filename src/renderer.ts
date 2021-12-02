c4dbb305e56d31744cc250cb11a6fa6236d21302
ryan-way <ryan.keith.way@gmail.com>
2021-12-01 21:00:11 -0800

    eslint fix and updating rule

--- src/renderer.ts
+++ src/renderer.ts
@@ -3,10 +3,10 @@ import './global.css';
 import App from './App.svelte';
 
 const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
+  target: document.body,
+  props: {
+    name: 'world',
+  },
 });
 
 export default app;
