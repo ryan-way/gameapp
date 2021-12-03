d053b4bd72925af2712fe09cc32da7475a684e44
ryan-way <ryan.keith.way@gmail.com>
2021-12-02 23:10:19 -0800

    full jest integration with jest; could be simplified

---
+++ src/__test__/hello.test.ts
@@ -0,0 +1,12 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { render } from '@testing-library/svelte';
+import App from '../App.svelte';
+
+test('should render', () => {
+  const results = render(App, { props: { name: 'world' } });
+
+  expect(() => results.getByText('Hello world!')).not.toThrow();
+});
