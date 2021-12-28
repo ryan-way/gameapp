ba028576b5f9973beaa08c493c7c7bed12c8d361
ryan-way <ryan.keith.way@gmail.com>
2021-12-28 13:11:42 -0800

    return renderer module to industry standard name

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import path from 'path';
 // by Forge's Webpack plugin that tells the Electron app where to look for the
 // Webpack-bundled app code (depending on whether you're running in development
 // or production).
-declare const RENDERER_WEBPACK_ENTRY: string;
+declare const MAIN_WINDOW_WEBPACK_ENTRY: string;
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
@@ -26,7 +26,7 @@ const createWindow = (): void => {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadURL(RENDERER_WEBPACK_ENTRY);
+  mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
