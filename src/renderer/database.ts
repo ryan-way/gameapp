e4e1de2319ab60ed6e28512f824d172ec40e592a
ryan-way <ryan.keith.way@gmail.com>
2022-02-12 17:32:09 -0800

    cleaning up renderer database api

--- src/renderer/database.ts
+++ src/renderer/database.ts
@@ -1,33 +1,25 @@
 import { ipcRenderer, contextBridge } from 'electron';
-import type { Sudoku } from '../data/sudoku';
-import type { Test } from '../data/test';
 import type { IDatabase } from '../service/database';
 
 export class Database implements IDatabase {
   public static db: IDatabase;
-  getTestEntities(): Promise<Test.Test[]> {
-    return ipcRenderer.invoke('getAllTest');
-  }
+  entities: Function[];
 
-  getTestEntity(id: number): Promise<Test.Test> {
-    return ipcRenderer.invoke('getOneTest', id);
-  }
-
-  getSudokuEntities(): Promise<Sudoku.Sudoku[]> {
-    return ipcRenderer.invoke('getAllSudoku');
-  }
-
-  getSudokuEntity(id: number): Promise<Sudoku.Sudoku> {
-    return ipcRenderer.invoke('getOneSudoku', id);
+  GetRepository<T>(func: new () => T) {
+    return {
+      GetOne: (id: number): Promise<T> => {
+        return ipcRenderer.invoke('getOne' + func.name, id);
+      },
+      GetAll: (): Promise<T[]> => {
+        return ipcRenderer.invoke('getAll' + func.name);
+      },
+    };
   }
 }
 
 export function InitializeDatabase() {
   Database.db = new Database();
-  contextBridge.exposeInMainWorld('db', {
-    getTestEntities: Database.db.getTestEntities.bind(this),
-    getTestEntity: Database.db.getTestEntity.bind(this),
-    getSudokuEntities: Database.db.getSudokuEntities.bind(this),
-    getSudokuEntity: Database.db.getSudokuEntity.bind(this),
+  contextBridge.exposeInMainWorld('data', {
+    GetRepository: Database.db.GetRepository.bind(this),
   });
 }
--- src/service/database.ts
+++ src/service/database.ts
@@ -1,11 +1,10 @@
-import type { Sudoku } from '../data/sudoku';
-import type { Test } from '../data/test';
-
 export interface IDatabase {
-  getTestEntities(): Promise<Test.Test[]>;
-  getTestEntity(id: number): Promise<Test.Test>;
-  getSudokuEntities(): Promise<Sudoku.Sudoku[]>;
-  getSudokuEntity(id: number): Promise<Sudoku.Sudoku>;
+  GetRepository<T>(func: new () => T): IRepository<T>;
+}
+
+export interface IRepository<T> {
+  GetOne(id: number): Promise<T>;
+  GetAll(): Promise<T[]>;
 }
 
 export const key: string = 'db';
