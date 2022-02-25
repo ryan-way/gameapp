d842cabf63d775a6f5e2e89d848c41309f47a102
ryan-way <ryan.keith.way@gmail.com>
2022-02-25 08:35:16 -0800

    Adding tests for Sudokus plus clean up

--- test/Sudokus.test.ts
+++ test/Sudokus.test.ts
@@ -2,34 +2,38 @@
  * @jest-environment jsdom
  */
 
-import { render } from '@testing-library/svelte';
+import { render, RenderResult } from '@testing-library/svelte';
 import Sudokus from '../src/sudoku/Sudokus.svelte';
 import testdata from '../src/main/testdata';
-import { overrides } from '../src/stores/service';
 import { mock, instance, when } from 'ts-mockito';
-import type { IDatabase, IRepository } from '../src/service/database';
-import { Sudoku, instance as inst } from '../src/data/sudoku';
+import { Sudoku } from '../src/data/sudoku';
+import { SudokuRepository } from '../src/repository/sudoku';
 
-const sudokus = testdata.get(Sudoku.Sudoku.name).map((board, idx) => {
-  return { id: idx, board: board } as unknown as Sudoku.Sudoku;
+let results: RenderResult;
+let testResolve: (reason?: any) => void;
+let testReject: (reason?: any) => void;
+
+beforeEach(() => {
+  const promise = new Promise<Sudoku.Sudoku[]>((resolve, reject) => {
+    testResolve = resolve;
+    testReject = reject;
+  });
+  const mockedRepo = mock(SudokuRepository);
+  when(mockedRepo.GetAll()).thenReturn(promise);
+  results = render(Sudokus, { repo: instance(mockedRepo) });
 });
 
-const promise = Promise.resolve(sudokus);
-
-const mockedRepo: IRepository<Sudoku.Sudoku> =
-  mock<IRepository<Sudoku.Sudoku>>();
-when(mockedRepo.GetAll()).thenReturn(promise);
-const repo: IRepository<Sudoku.Sudoku> = instance(mockedRepo);
-
-const mockedData: IDatabase = mock<IDatabase>();
-when(mockedData.GetRepository<Sudoku.Sudoku>(inst)).thenReturn(repo);
-const data: IDatabase = instance(mockedData);
-overrides.data.set(data);
-
-const results = render(Sudokus);
-
 test('should render Sudokus', () => {
+  const sudokus = testdata.get(Sudoku.Sudoku.name).map((board, idx) => {
+    return { id: idx, board: board } as unknown as Sudoku.Sudoku;
+  });
+  testResolve(sudokus);
   for (let i = 1; i <= sudokus.length; i++) {
     expect(() => results.findByText(1)).not.toThrow();
   }
+});
+
+test('should render Error', () => {
+  testReject(new Error('Test Error'));
+  expect(() => results.findAllByText('Test Error'));
 });
