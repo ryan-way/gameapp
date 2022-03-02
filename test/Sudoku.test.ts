36bb95f3bc0686765e6402c02419916629cb3d0d
ryan-way <ryan.keith.way@gmail.com>
2022-03-02 14:46:10 -0800

    MockTest for updates sudoku puzzle

--- test/Sudoku.test.ts
+++ test/Sudoku.test.ts
@@ -2,12 +2,13 @@
  * @jest-environment jsdom
  */
 
-import { render } from '@testing-library/svelte';
+import { getByText, render } from '@testing-library/svelte';
 import { Sudoku as Entity } from '../src/data/sudoku';
 import testdata from '../src/main/testdata';
 import { mock, instance, when } from 'ts-mockito';
 import { SudokuRepository } from '../src/repository/sudoku';
 import Sudoku from '../src/sudoku/Sudoku.svelte';
+import { fireEvent } from '@testing-library/dom';
 
 test('should render Sudoku', () => {
   const sudoku = testdata
@@ -35,4 +36,25 @@ test('should show error', () => {
   when(mockedRepo.GetOne(1)).thenReject(new Error('Test Error'));
   const results = render(Sudoku, { repo: instance(mockedRepo), id: 1 });
   expect(() => results.findAllByText('Test Error')).not.toThrow();
+});
+
+test('should solve one', async () => {
+  const sudoku = testdata
+    .get(Entity.Sudoku.name)
+    .map((board, idx) => {
+      return { id: idx, board: board } as unknown as Entity.Sudoku;
+    })
+    .at(0);
+  const mockedRepo = mock(SudokuRepository);
+  when(mockedRepo.GetOne(1)).thenResolve(sudoku);
+  const { findAllByText, getByText } = render(Sudoku, {
+    repo: instance(mockedRepo),
+    id: 1,
+  });
+  const { length } = await findAllByText('', { selector: 'td' });
+  const button = getByText('Solve');
+  await fireEvent.click(button);
+  const cells = await findAllByText('', { selector: 'td' });
+
+  expect(cells.length).toBe(length - 1);
 });
