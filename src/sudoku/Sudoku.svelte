84087931aaec91fcaf861d4e212bc1b2398ec3ff
ryan-way <ryan.keith.way@gmail.com>
2022-03-19 17:31:44 -0700

    Sudoku check implemented

--- src/sudoku/Sudoku.svelte
+++ src/sudoku/Sudoku.svelte
@@ -4,17 +4,22 @@
   import Board from '../components/Board.svelte';
   import type { Sudoku } from '../data/sudoku';
   import { SudokuRepository } from '../repository/sudoku';
+  import { SudokuChecker } from '../validation/sudokuchecker';
 
   export let repo = new SudokuRepository();
   export let id: number;
 
   let game: Sudoku.Sudoku;
   let solver: SudokuSolver;
+  let checker: SudokuChecker;
   let showCandidates = true;
-  let boardStyle = 'height: 100%; width: 49%; vertical-align: top;';
+  let boardStyle = 'height: 100%; width: 49%; vertical-align: center;';
+  let howAmIDoing = '';
+  let selectedNum = 1;
 
   onMount(async () => {
     game = await repo.GetOne(id);
+    checker = new SudokuChecker(game.board);
     solver = new SudokuSolver(game.board);
     solver.Solve();
   });
@@ -38,35 +43,78 @@
     }
   }
 
-  function Check() {}
+  function Check() {
+    howAmIDoing = '...checking';
+    setTimeout(() => {
+      if (checker.Check()) {
+        howAmIDoing = 'Great!';
+      }
+      else {
+        howAmIDoing = 'Errors Found';
+      }
+      setTimeout(Clear, 1000);
+    }, 1000);
+  }
+
+  function Clear() {
+    howAmIDoing = '';
+  }
+
+  function UpdateSelectedNum({ key }) {
+    if (isNaN(+key)) return;
+    if (+key > 9 || +key < 1) return;
+    selectedNum = +key;
+    console.log(typeof selectedNum);
+  }
+
+  function onCellClick(cell) {
+    cell.Value = selectedNum;
+    game.board = game.board;
+  }
 </script>
+
+<svelte:window on:keydown={UpdateSelectedNum} />
 
 {#if game}
   <div>
-    <Board style={boardStyle}>
-      {#each game.board.flat() as cell, idx}
-        <td class="solved" data-testid="Board{Math.floor(idx / 9)}{idx % 9}">
-          {cell.Value}
-        </td>
-      {/each}
-    </Board>
-    {#if showCandidates}
+    <div >
       <Board style={boardStyle}>
-        {#each solver.Board.flat() as cell}
-          <td class="candidate">
-            {#each cell.Candidates as candidate, idx}
-              {candidate}
-              {#if (idx + 1) % 3 == 0}<br />{/if}
-            {/each}
+        {#each game.board.flat() as cell, idx}
+          <td class="solved" data-testid="Board{Math.floor(idx / 9)}{idx % 9}"
+          on:click={() => onCellClick(cell)}>
+            {cell.Value}
           </td>
         {/each}
       </Board>
-    {/if}
+      {#if showCandidates}
+        <Board style={boardStyle}>
+          {#each solver.Board.flat() as cell}
+            <td class="candidate">
+              {#each cell.Candidates as candidate, idx}
+                {candidate}
+                {#if (idx + 1) % 3 == 0}<br />{/if}
+              {/each}
+            </td>
+          {/each}
+        </Board>
+      {/if}
+    </div>
+    <p style="font-size: 40px" >
+      {#each Array.from(Array(9).keys()).map(x => x+1) as num, idx}
+        <span class:selected={num == selectedNum} 
+        class="numpad" 
+        on:click={() => UpdateSelectedNum({ key: `${num}`})}>
+          {num}
+        </span>
+          {#if (idx + 1) % 3 == 0}<br />{/if}
+      {/each}
+    </p>
   </div>
   <div>
     <button on:click={Solve}>Solve</button>
     <button on:click={Hint}>Hint</button>
     <button on:click={Check}>How Am I Doing?</button>
+    {#if howAmIDoing} {howAmIDoing} {/if}
     <input type="checkbox" bind:checked={showCandidates} />
     Show Ai Table
   </div>
@@ -75,7 +123,19 @@
 {/if}
 
 <style>
+  span.selected {
+    color: black;
+    background: orange;
+  }
+  span.numpad {
+    padding-left: 10px;
+    padding-right: 10px;
+    border-radius: 50%;
+    border: 1px red;
+  }
   div {
+    display: inline;
+    align-content: center;
     align-items: center;
   }
   button {
--- src/validation/sudokuchecker.ts
+++ src/validation/sudokuchecker.ts
@@ -1,1 +1,48 @@
-export {};
+import { CandidatedCell } from '../ai/candidatedcell';
+import type { Cell } from '../data/cell';
+import type { Sudoku } from '../data/sudoku';
+import { UniqueValues } from './rules';
+
+export class SudokuChecker {
+  private board: CandidatedCell<Sudoku.Value>[][];
+  private regions: CandidatedCell<Sudoku.Value>[][];
+  private validators: UniqueValues<Sudoku.Value>[];
+  constructor(board: Cell<Sudoku.Value>[][]) {
+    this.SetupBoard(board);
+    this.SetupRegions();
+    this.SetupValidators();
+  }
+
+  SetupBoard(board: Cell<Sudoku.Value>[][]) {
+    const domain: Sudoku.Value[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    this.board = board.map(row =>
+      row.map(cell => new CandidatedCell(cell, ' ', domain))
+    );
+  }
+
+  SetupRegions() {
+    this.regions = [...this.board];
+    for (let i = 0; i < this.board[0].length; i++) {
+      this.regions.push(this.board.map(row => row[i]));
+    }
+
+    for (let i = 0; i < 9; i++) {
+      const row: number = Math.floor(i / 3) * 3;
+      const column: number = (i % 3) * 3;
+      this.regions.push(
+        this.board
+          .slice(row, row + 3)
+          .map(row => row.slice(column, column + 3))
+          .flat()
+      );
+    }
+  }
+
+  SetupValidators() {
+    this.validators = this.regions.map(region => new UniqueValues(region));
+  }
+
+  Check(): boolean {
+    return this.validators.every(validator => validator.Check());
+  }
+}
