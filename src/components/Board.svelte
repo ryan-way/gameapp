435952db03e3063011a20737f31ff1019b835bd3
ryan-way <ryan.keith.way@gmail.com>
2022-03-19 17:31:24 -0700

    board component refactor

--- src/components/Board.svelte
+++ src/components/Board.svelte
@@ -1,7 +1,4 @@
 <script lang="ts">
-  import { createEventDispatcher } from 'svelte';
-  import type { Cell } from '../data/cell';
-
   export let rows: number = 9;
   export let columns: number = 9;
   export let style: string;
@@ -9,14 +6,6 @@
   let internalStyle = `grid-template-columns: repeat(${columns}, 1fr);
                grid-template-rows: repeat(${rows}, 1fr);
                border: 0.25px solid;`;
-
-  const dispatch = createEventDispatcher();
-
-  function onCellClick(cell: Cell<any>) {
-    dispatch('cellClick', {
-      cell: cell,
-    });
-  }
 </script>
 
 <span style={style + internalStyle}>
